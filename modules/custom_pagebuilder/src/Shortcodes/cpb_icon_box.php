1db7d10d4981599e9fa9265876dd13f768d5a66d
Chau Phan <truongchau007@gmail.com>
2017-04-18 21:54:27 +0700

    Fix Icon Box

--- modules/custom_pagebuilder/src/Shortcodes/cpb_icon_box.php
+++ modules/custom_pagebuilder/src/Shortcodes/cpb_icon_box.php
@@ -23,20 +23,31 @@ if(!class_exists('cpb_icon_box')):
                ),
          
                array(
-                  'id'        => 'icon',
-                  'type'      => 'text',
-                  'title'     => t('Icon class'),
-                  'std'       => '',
+                  'id'           => 'add_icon',
+                  'type'         => 'checkboxs',
+                  'title'        => t('Use icon ?'),
+                  'options'      => array(
+                    'add_icon'  => 'add_icon', 
+                  ), 
+                  'default_value' => 'add_icon',
                ),
+              
+              array(
+                  'id'           => 'chose_icon',
+                  'type'         => 'icon_font',
+                  'title'        => t('Chose Icon'), 
+              ),
+              
                array(
                   'id'        => 'icon_size',
                   'type'      => 'select',
                   'title'     => 'Icon font size',
                   'options'   => array(''=>'Default', 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60)
                ),
+              
                array(
                   'id'        => 'icon_color',
-                  'type'      => 'text',
+                  'type'      => 'color',
                   'title'     => t('Icon Color'),
                   'desc'      => t('Color for icon, e.g: #000'),
                   'std'       => '',
@@ -72,7 +83,7 @@ if(!class_exists('cpb_icon_box')):
 
                array(
                   'id'        => 'bg_color',
-                  'type'      => 'text',
+                  'type'      => 'color',
                   'title'     => t('Background color'),
                   'desc'      => t('Background for icon box, e.g: #f5f5f5')
                ),
@@ -90,7 +101,7 @@ if(!class_exists('cpb_icon_box')):
                array(
                   'id'        => 'target',
                   'type'      => 'select',
-                  'options'   => array( 'on' => 'No', 'off' => 'Yes' ),
+                  'options'   => array( 'off' => 'No', 'on' => 'Yes' ),
                   'title'     => t('Open in new window'),
                   'desc'      => t('Adds a target="_blank" attribute to the link.'),
                ),
@@ -98,11 +109,27 @@ if(!class_exists('cpb_icon_box')):
                array(
                   'id'        => 'animate',
                   'type'      => 'select',
-                  'title'     => t('Animation'),
+                  'title'     => t('Animation Icon'),
                   'desc'      => t('Entrance animation for element'),
                   'options'   => custom_pagebuilder_animate(),
                ),
                
+              array(
+                'id'    => 'duration',
+                'type'    => 'text',
+                'title'   => ('Anumate Duration'),
+                'desc'    => ('Change the animation duration'),
+                'class'   => 'small-text',
+              ),
+
+             array(
+                'id'    => 'delay',
+                'type'    => 'text',
+                'title'   => ('Anumate Delay'),
+                'desc'    => ('Delay before the animation starts'),
+                'class'   => 'small-text',
+              ),
+              
                array(
                   'id'     => 'el_class',
                   'type'      => 'text',
@@ -117,84 +144,91 @@ if(!class_exists('cpb_icon_box')):
 
       public function render_content( $item ) {
          if( ! key_exists('content', $item['fields']) ) $item['fields']['content'] = '';
-         print self::sc_icon_box( $item['fields'], $item['fields']['content'] );
+         return self::sc_icon_box( $item['fields'], $item['fields']['content'] );
       }
 
 
       public static function sc_icon_box( $attr, $content = null ){
          global $base_url;
-         extract(shortcode_atts(array(
-            'title'           => '',
-            'subtitle'        => '',
-            'icon'            => '',
-            'icon_size'       => '',
-            'icon_color'      => '',
-            'image'           => '',
-            'icon_position'   => 'top',
-            'link'            => '',
-            'skin_text'       => '',
-            'target'          => '',
-            'animate'         => '',
-            'el_class'        => '',
-            'bg_color'        => ''
-         ), $attr));
 
 
          // target
-         if( $target ){
+         if( $attr['target'] == 'on' ){
             $target = 'target="_blank"';
          } else {
             $target = false;
          }
 
-         if($image){
-            $image = $base_url . '/' .$image; 
+         $icon_content = '';
+         if(!empty($attr['add_icon'])) {
+           $icon_content = '<i style="{{ style_icon }}" class="'. $attr['chose_icon'] .'"></i>';
+         } elseif(isset($attr['image'])) {
+           $alt = (isset($attr['alt_img'])) ? $attr['alt_img'] : '';
+           $icon_content = '<img src="'. $attr['image'] .'" alt="'. $alt .'" />';
          }
-
+         
+         // Amination Box
+         $class_animate = '';
+         $duration = '';
+         $delay = '';
+         if(!empty($attr['animate'])) {
+           $class_animate = ' wow '. $attr['animate'];
+           $duration = !empty($attr['duration']) ? 'data-wow-duration='.$attr['duration'].'s' : '';
+           $delay = !empty($attr['delay']) ? 'data-wow-delay='.$attr['delay'].'s' : '';
+         }
+         
+         // Class Box
          $class = array();
-         if($el_class){ $class[] = $el_class; }
-         $class[] = $icon_position;
-         if($skin_text){$class[] = $skin_text;}
+         if($attr['el_class']){ $class[] = $attr['el_class']; }
+         $class[] = $attr['icon_position'];
+         if($attr['skin_text']){
+           $class[] = $attr['skin_text'];
+         }
 
          //Background color
          $style = array();
-         if($bg_color){
-            $style[] = 'background-color: ' . $bg_color;
+         if($attr['bg_color']){
+            $style[] = 'background-color: ' . $attr['bg_color'];
          }
+         
+         
          $style_icon = array();
-         if($icon_size){
-            $style_icon[] = 'font-size: ' . (14 + 2*$icon_size) .'px';
+         if($attr['icon_size']){
+            $style_icon[] = 'font-size: ' . (14 + 2*$attr['icon_size']) .'px';
          }
-         if($icon_color){
-            $style_icon[] = 'color: ' . $icon_color;
+         if($attr['icon_color']){
+            $style_icon[] = 'color: ' . $attr['icon_color'];
          }
-
-         ?>
-
-         <div class="widget gsc-icon-box <?php if(count($class)>0) print implode($class, ' ') ?>" <?php if(count($style) > 0) print 'style="'.implode($style, ';').'"' ?>>
-            
-            <?php if($image){ ?>
-               <div class="highlight-image"><img src="<?php print $image ?>" alt=""/></div>
-            <?php }?>
-
-            <?php if($icon){ ?>
-               <div class="highlight-icon"><span class="icon <?php print $icon ?>"<?php if(count($style_icon) > 0) print ' style="'.implode($style_icon, ';').'"' ?>>&nbsp;</span></div>
-            <?php }?>
-
-            <div class="highlight_content">
-               <h4><?php print $title; ?></h4>
-               <?php if($subtitle){ ?>
-                  <div class="subtitle"><?php print $subtitle; ?></div>
-               <?php } ?>
-               <div class="desc"><?php print do_shortcode($content); ?></div>
-            </div>
-         </div>
-
-       <?php
-      }
-
-      public function load_shortcode(){
-         add_shortcode( 'icon_box', array('cpb_icon_box', 'sc_icon_box') );
+         
+         
+         
+         $output = '<div style="{{ style_box }}" class="wrapper-custom-pagebuilder-icon-box '. implode($class, ' ') .' ">';
+            $output .= '<div class="inner-icon-box">';
+            if(!empty($icon_content)) {
+              $output .= '<div class="highlight-image-icon">'
+                  . '<div class="icon-content '. $class_animate .'" {{ duration }} {{ delay }} >'. $icon_content .'</div></div>';
+            }
+            $output .= '<div class="highlight_content">';
+              if($attr['title']) {
+                $output .= '<h3>'. $attr['title'] .'</h3>';
+              }
+              if($attr['content']) {
+                $output .= '<div class="desc">'. $attr['content'] .'</div>';
+              }
+            $output .= '</div>';
+            $output .= '</div>';
+         $output .= '</div>';
+         
+         return array(
+                    '#type' => 'inline_template',
+                    '#template' => $output,
+                    '#context' => array(
+                      'style_icon' => implode(";", $style_icon),
+                      'duration' => $duration,
+                      'style_box' => implode(";", $style),
+                    ),
+                  ); 
+         
       }
    }
 endif;   
