ac1f93b8f4b28ba906e2849eeb5bed79e68d71bf
Chau Phan <truongchau007@gmail.com>
2017-05-09 16:51:19 +0700

    Up

---
+++ themes/xinhvnn_base/js/main.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+jQuery(document).ready(function($) {
+
+    $(".btn").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        var wrapper = $(this).closest('.wrapper-portfolio-content');
+        wrapper.find('.portfolio-content').fadeTo(100, 0.1);
+        wrapper.find('.portfolio-content div').not("." + selectedClass).fadeOut().removeClass("scale-anm");
+
+        setTimeout(function () {
+            wrapper.find('.portfolio-content .' + selectedClass).fadeIn().addClass("scale-anm");
+                    wrapper.find('.portfolio-content').fadeTo(300, 1);
+        }, 300);
+    });
+
+});
